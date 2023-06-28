829e6cdf5285c7a40586328103e1f17f1b8bb32f
darklyspaced <srohanjd@gmail.com>
2023-06-28 15:33:33 +0800

    fleshed out error types

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,8 @@
+use std::fmt::Debug;
+use std::fmt::Write;
 use thiserror::Error;
+
+use crate::token::Type;
 
 /// Error that can be generated during the lexing phase of the interpreter.
 #[derive(Error, Debug)]
@@ -8,4 +12,20 @@ pub enum SyntaxError {
 }
 /// Error that is generated during interpretation.
 #[derive(Error, Debug)]
-pub enum RuntimeError {}
+pub enum RuntimeError {
+    #[error("an invalid operator was used. found: {0} expected: {}", display_vec(.1))]
+    InvalidOperator(String, Vec<char>),
+    #[error("an invalid type was found: {0}; expected: {}", display_vec(.1))]
+    InvalidType(Type, Vec<Type>),
+    #[error("cannot apply {0} to values ({}); expected: {}", display_vec(.1), display_vec(.2))]
+    InvalidTypes(String, Vec<Type>, Vec<Type>),
+}
+
+fn display_vec<'a, T>(vec: &Vec<T>) -> String
+where
+    T: Debug,
+{
+    let mut buffer = String::new();
+    write!(buffer, "{:?}", vec).unwrap();
+    String::from(&buffer[1..buffer.len() - 2])
+}
