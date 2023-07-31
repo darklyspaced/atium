1e4136779c517f425f2f3bb692a3b6007119e21f
darklyspaced <srohanjd@gmail.com>
2023-07-31 12:21:31 +0800

    extracted lexer functions into standalone functions

--- src/lexer.rs
+++ src/lexer.rs
@@ -15,6 +15,8 @@ pub(super) struct Cursor<'a> {
     tokens: Vec<Token>,
     /// reserved keywords for the language
     reserved: HashMap<String, TokenType>,
+    /// errors present in the source code
+    errors: Vec<Report>,
     /// line that the cursor is currently lexing
     line: usize,
 }
@@ -24,6 +26,7 @@ impl<'a> Cursor<'a> {
         Self {
             iter: src.chars().peekable(),
             tokens: Vec::default(),
+            errors: Vec::default(),
             reserved: HashMap::from([
                 (String::from("and"), And),
                 (String::from("class"), Class),
@@ -52,134 +55,43 @@ impl<'a> Cursor<'a> {
     }
 
     pub fn scan_tokens(mut self) -> Result<Vec<Token>, Vec<Report>> {
-        let mut errors: Vec<Report> = vec![];
-
         loop {
             let next = self.iter.next();
-            if let Some(chr) = next {
-                match chr {
-                    '(' => self.add_token(LeftParen, chr.to_string(), None),
-                    ')' => self.add_token(RightParen, chr.to_string(), None),
-                    '{' => self.add_token(LeftBrace, chr.to_string(), None),
-                    '}' => self.add_token(RightBrace, chr.to_string(), None),
-                    ',' => self.add_token(Comma, chr.to_string(), None),
-                    '.' => self.add_token(Dot, chr.to_string(), None),
-                    '-' => self.add_token(Minus, chr.to_string(), None),
-                    '+' => self.add_token(Plus, chr.to_string(), None),
-                    ';' => self.add_token(Semicolon, chr.to_string(), None),
-                    '*' => self.add_token(Star, chr.to_string(), None),
-                    '!' => self.handle_two_char_op(chr, '=', BangEqual, Bang),
-                    '=' => self.handle_two_char_op(chr, '=', EqualEqual, EqualEqual),
-                    '<' => self.handle_two_char_op(chr, '=', LessEqual, Less),
-                    '>' => self.handle_two_char_op(chr, '=', GreaterEqual, Greater),
-                    '/' => {
-                        dbg!(&self.iter.peek());
-                        if self.iter.peek().unwrap() == &'/' {
-                            loop {
-                                match self.iter.next() {
-                                    Some('\n') | None => break,
-                                    Some(_) => (),
-                                }
-                            }
-                            self.line += 1;
-                        } else {
-                            self.add_token(Slash, chr.to_string(), None);
-                        }
-                    }
-                    '"' => {
-                        let mut chars = vec!['"'];
-                        let (token, lit) = loop {
-                            match self.iter.next() {
-                                Some('"') => break (Str, chars[1..].iter().collect::<String>()),
-                                Some(char) => chars.push(char),
-                                None => errors.push(
-                                    SyntaxError::ExpectedCharacter(String::from("EOF"), '"').into(),
-                                ),
-                            }
-                        };
-
-                        chars.push('"');
-                        self.add_token(
-                            token,
-                            chars.into_iter().collect::<String>(),
-                            Some(Value::String(lit)),
-                        );
-                    }
-                    _ if chr.is_ascii_digit() => {
-                        let mut num = vec![chr];
-                        let mut float = false;
-
-                        loop {
-                            match self.iter.peek() {
-                                Some('0'..='9') => num.push(self.iter.next().unwrap()),
-                                Some('.') => {
-                                    float = true;
-                                    num.push(self.iter.next().unwrap());
-                                }
-                                _ => break,
-                            }
-                        }
-
-                        let pre_literal = num.into_iter().collect::<String>();
-                        let lexeme = pre_literal.clone();
-
-                        if float {
-                            self.add_token(
-                                Number,
-                                lexeme,
-                                Some(Value::Float(pre_literal.parse::<f64>().unwrap())),
-                            );
-                        } else {
-                            self.add_token(
-                                Number,
-                                lexeme,
-                                Some(Value::Integer(pre_literal.parse::<i128>().unwrap())),
-                            );
-                        }
-                    }
-                    'a'..='z' | 'A'..='Z' => {
-                        let mut ident = vec![chr];
-                        while let Some('a'..='z' | 'A'..='Z' | '1'..='9') = self.iter.peek() {
-                            ident.push(self.iter.next().unwrap());
-                        }
-                        // loop {
-                        //     match self.iter.peek() {
-                        //         Some(char) if char.is_alphanumeric() || char.eq(&'_') => {
-                        //             ident.push(self.iter.next().unwrap());
-                        //         }
-                        //         _ => break,
-                        //     }
-                        // }
-                        let ident = ident.into_iter().collect::<String>();
-                        if self.reserved.contains_key(&ident) {
-                            let tt = self.reserved.get(&ident).unwrap().clone();
-                            match tt {
-                                TokenType::True => {
-                                    self.add_token(tt, ident, Some(Value::Boolean(true)));
-                                }
-                                TokenType::False => {
-                                    self.add_token(tt, ident, Some(Value::Boolean(false)));
-                                }
-                                _ => self.add_token(tt, ident, None),
-                            }
-                        } else {
-                            self.add_token(Identifier, ident, None);
-                        }
-                    }
+            if let Some(curr) = next {
+                match curr {
+                    '(' => self.add_token(LeftParen, curr.to_string(), None),
+                    ')' => self.add_token(RightParen, curr.to_string(), None),
+                    '{' => self.add_token(LeftBrace, curr.to_string(), None),
+                    '}' => self.add_token(RightBrace, curr.to_string(), None),
+                    ',' => self.add_token(Comma, curr.to_string(), None),
+                    '.' => self.add_token(Dot, curr.to_string(), None),
+                    '-' => self.add_token(Minus, curr.to_string(), None),
+                    '+' => self.add_token(Plus, curr.to_string(), None),
+                    ';' => self.add_token(Semicolon, curr.to_string(), None),
+                    '*' => self.add_token(Star, curr.to_string(), None),
+                    '!' => self.handle_two_char_op(curr, '=', BangEqual, Bang),
+                    '=' => self.handle_two_char_op(curr, '=', EqualEqual, EqualEqual),
+                    '<' => self.handle_two_char_op(curr, '=', LessEqual, Less),
+                    '>' => self.handle_two_char_op(curr, '=', GreaterEqual, Greater),
+                    '/' => self.handle_comment(curr),
+                    '"' => self.handle_string(),
+                    '0'..='9' => self.handle_number(curr),
+                    'a'..='z' | 'A'..='Z' => self.handle_ident(curr),
                     '\n' => self.line += 1,
                     '\r' | '\t' | ' ' => (),
-                    _ => errors.push(SyntaxError::UnexpectedCharacter(chr).into()),
+                    _ => self
+                        .errors
+                        .push(SyntaxError::UnexpectedCharacter(curr).into()),
                 };
             } else {
                 break;
             }
         }
 
-        dbg!(&self.tokens);
-        if errors.is_empty() {
+        if self.errors.is_empty() {
             Ok(self.tokens)
         } else {
-            Err(errors)
+            Err(self.errors)
         }
     }
 
@@ -197,6 +109,96 @@ impl<'a> Cursor<'a> {
             }
             Some(_) => self.add_token(failure, predicate.to_string(), None),
             None => (), // NOTE: we now can add smthing to None case in the future
+        }
+    }
+
+    pub fn handle_ident(&mut self, curr: char) {
+        let mut ident = vec![curr];
+        while let Some('a'..='z' | 'A'..='Z' | '1'..='9') = self.iter.peek() {
+            ident.push(self.iter.next().unwrap());
+        }
+
+        let ident = ident.into_iter().collect::<String>();
+        if self.reserved.contains_key(&ident) {
+            let tt = self.reserved.get(&ident).unwrap().clone();
+            match tt {
+                TokenType::True => {
+                    self.add_token(tt, ident, Some(true.into()));
+                }
+                TokenType::False => {
+                    self.add_token(tt, ident, Some(false.into()));
+                }
+                _ => self.add_token(tt, ident, None),
+            }
+        } else {
+            self.add_token(Identifier, ident, None);
+        }
+    }
+
+    pub fn handle_number(&mut self, curr: char) {
+        let mut num = vec![curr];
+        let mut float = false;
+
+        loop {
+            match self.iter.peek() {
+                Some('0'..='9') => num.push(self.iter.next().unwrap()),
+                Some('.') => {
+                    float = true;
+                    num.push(self.iter.next().unwrap());
+                }
+                _ => break,
+            }
+        }
+
+        let pre_literal = num.into_iter().collect::<String>();
+        let lexeme = pre_literal.clone();
+
+        if float {
+            self.add_token(
+                Number,
+                lexeme,
+                Some(Value::Float(pre_literal.parse::<f64>().unwrap())),
+            );
+        } else {
+            self.add_token(
+                Number,
+                lexeme,
+                Some(Value::Integer(pre_literal.parse::<i128>().unwrap())),
+            );
+        }
+    }
+
+    pub fn handle_string(&mut self) {
+        let mut chars = vec!['"'];
+        let (token, lit) = loop {
+            match self.iter.next() {
+                Some('"') => break (Str, chars[1..].iter().collect::<String>()),
+                Some(char) => chars.push(char),
+                None => self
+                    .errors
+                    .push(SyntaxError::ExpectedCharacter(String::from("EOF"), '"').into()),
+            }
+        };
+
+        chars.push('"');
+        self.add_token(
+            token,
+            chars.into_iter().collect::<String>(),
+            Some(Value::String(lit)),
+        );
+    }
+
+    pub fn handle_comment(&mut self, curr: char) {
+        if self.iter.peek().unwrap() == &'/' {
+            loop {
+                match self.iter.next() {
+                    Some('\n') | None => break,
+                    Some(_) => (),
+                }
+            }
+            self.line += 1;
+        } else {
+            self.add_token(Slash, curr.to_string(), None);
         }
     }
 }
@@ -334,31 +336,31 @@ mod tests {
                 Token {
                     token_type: TokenType::True,
                     lexeme: String::from("true"),
-                    literal: Some(Value::Boolean(true,),),
+                    literal: Some(true.into(),),
                     line: 0,
                 },
                 Token {
                     token_type: TokenType::Number,
                     lexeme: String::from("1"),
-                    literal: Some(Value::Integer(1,),),
+                    literal: Some(1.into(),),
                     line: 0,
                 },
                 Token {
                     token_type: TokenType::Str,
                     lexeme: String::from("\"foo\""),
-                    literal: Some(Value::String(String::from("foo"),),),
+                    literal: Some(String::from("foo").into()),
                     line: 0,
                 },
                 Token {
                     token_type: TokenType::False,
                     lexeme: String::from("false"),
-                    literal: Some(Value::Boolean(false,),),
+                    literal: Some(false.into(),),
                     line: 0,
                 },
                 Token {
                     token_type: TokenType::Number,
                     lexeme: String::from("69.420"),
-                    literal: Some(Value::Float(69.420),),
+                    literal: Some(69.420.into(),),
                     line: 0,
                 }
             ]
